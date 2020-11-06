b3e1bfdf2c8f00ab95479b9112008bba1754f7d2
Archangel <nathapcal@gmail.com>
2020-11-05 22:57:12 -0500

    created seed data for testing

--- models/book.js
+++ models/book.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema
 const bookSchema = new Schema ({
     title: {type: String, required: true},
     author: {type: String, require: true},
-    description: String, 
-    date: {type: Date, default: Date.now},
+    description: {type: String}, 
+    // date: {type: Date, default: Date.now},
     image: {type: String},
-    link: String
+    link: {type: String}
 })
 
 const Book = mongoose.model ("Book", bookSchema)
